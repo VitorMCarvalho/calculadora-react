f8815c3d99b3c761c87cc429522a655ab44751b9
vitor <mendes.vitor2104@gmail.com>
2023-03-21 23:42:23 -0300

    #feat finalizando logica da calculadora

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,33 +3,87 @@ import "./Calculator.css";
 import Button  from "../components/button";
 import Display from "../components/display";
 
+const initialState = {
+    displayValue: "0",
+    clearDisplay: false,
+    operation:null,
+    values: [0,0],
+    currentIndex:0
+};
+
 export default class Calculator extends Component{
 
     constructor(props){
         super(props);
-        this.clearMemory = this.clearMemory.bind(this)
-        this.setOperation = this.setOperation.bind(this)
-        this.addDigit = this.addDigit.bind(this)
+        this.clearMemory = this.clearMemory.bind(this);
+        this.setOperation = this.setOperation.bind(this);
+        this.addDigit = this.addDigit.bind(this);
     }
 
+    state = {...initialState};
+
     clearMemory(){
-        //todo
+        this.setState({...initialState});
     }
 
     setOperation(operator){
-        //todo
+        if(this.state.currentIndex === 0){
+            this.setState({
+                operation:operator, 
+                currentIndex: 1,
+                clearDisplay:true});
+        }else{
+            const equal = operator === "=";
+            const currentOp = this.state.operation;
+            const values = [...this.state.values];
+
+            try{
+                // eslint-disable-next-line no-eval
+                values[0] = eval(`${values[0]} ${currentOp} ${values[1]}`);
+                if (isNaN(values[0]) || !isFinite(values[0])) {
+                    this.clearMemory()
+                return
+                }
+            }catch(e){
+                values[0] = this.state.values[0]
+            }
+            
+            values[1] = 0;
+
+            this.setState({
+                displayValue:values[0],
+                operation:equal ? null : operator,
+                current: equal ? 0 : 1,
+                clearDisplay: !equal,
+                values
+            })
+        }
     }
 
     addDigit(digit){
-        //todo
+        if(digit === "." && this.state.displayValue.includes(".")){
+            return;
+        }
+
+        const clearDisplay = this.state.displayValue === "0" ||
+            this.state.clearDisplay;
+        const currentValue = clearDisplay ? "" : this.state.displayValue;
+        const displayValue = currentValue + digit;
+        this.setState({displayValue, clearDisplay:false});
+
+        if(digit !== "."){
+            const index = this.state.currentIndex;
+            const newNumber = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[index] = newNumber;
+            this.setState({values});
+        }
     }
-
-
 
     render(){
         return (
             <div className="calculator">
-                <Display value="100"></Display>
+                <Display value={this.state.displayValue}></Display>
                 <Button label="AC" click={this.clearMemory} triple/>
                 <Button label="/"  click={this.setOperation} operation/>
                 <Button label="7"  click={this.addDigit}/>
